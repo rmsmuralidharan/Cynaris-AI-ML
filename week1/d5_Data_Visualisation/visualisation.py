38a86011ab5f8d990a741be196c2832a0c6403ad
murali dharan <rmsmuralidharan@gmail.com>
2026-09-07 14:47:15 +0530

    feat: add basic employee data visualizations

---
+++ week1/d5_Data_Visualisation/visualisation.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## loading the celaned dataset that i've cleaned on day 3
+df = pd.read_csv(
+   "week1/d3_Data_Loading_Cleaning/data/cleaned_employee_performance.csv"
+)
+
+## display basic info about the dataset
+print('dataset shape:', df.shape)
+print('\nfirst 5 rows:')
+print(df.head(5))
+
+## department wise employee count
+plt.figure()
+sns.countplot(data=df, x='Department')
+plt.title('employee count by department')
+plt.xlabel('department')
+plt.ylabel('employee count')
+plt.xticks(rotation = 45)
+plt.tight_layout()
+plt.show()
+
+## salary distribution
+plt.figure()
+sns.histplot(data=df, x='Salary', kde=True)
+plt.title('Salary Distribution')
+plt.xlabel('Salary')
+plt.ylabel('Frequency')
+plt.tight_layout()
+plt.show()
+
+## performance wise distribution
+plt.figure()
+sns.histplot(data=df, x='Performance_Score', kde=True)
+plt.title('Performance score Distribution')
+plt.xlabel('performance score')
+plt.ylabel('Frequencey')
+plt.tight_layout()
+plt.show()
