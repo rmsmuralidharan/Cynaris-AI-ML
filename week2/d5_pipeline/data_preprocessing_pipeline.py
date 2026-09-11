6922fa885aa69ce622ae47456ed077584c8fded2
murali dharan <rmsmuralidharan@gmail.com>
2026-09-11 16:21:43 +0530

    feat (pipeline): data preprocessing completed

--- week2/d5_pipeline/data_preprocessing_pipeline.py
+++ week2/d5_pipeline/data_preprocessing_pipeline.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 
 ### loading the titanic dataset
 df = pd.read_csv('week2/d5_pipeline/data/sythetic_titanic_dataset.csv')
@@ -58,4 +59,59 @@ print(df.isnull().sum())
 ## 2.duplicate values
 print('\nduplicate values:')
 print(df.duplicated().sum())
+
+
+### removing the unwanted features
+df = df.drop(columns=['PassengerId', 'Name', 'Ticket'])
+print('\nfeatures after removing:')
+print(df.head(2))
+
+
+### Data preprocessing
+
+## 1. ordinal encoding for the sex feature
+ordinal_encoder = OrdinalEncoder(
+    categories=[['female', 'male']]
+)
+
+ordinal_encoded = ordinal_encoder.fit_transform(df[['Sex']])
+
+ordinal_df = pd.DataFrame(
+    ordinal_encoded
+)
+
+print('\nordinal encoding:')
+for category, value in enumerate(ordinal_encoder.categories_[0]):
+    print(f"{category} ---> {value}")
+
+
+
+### 2. one hot encoding for embarked and cabin
+
+onehot_encoder = OneHotEncoder(
+    sparse_output=False,
+    handle_unknown='ignore'
+)
+
+onehot_encoded = onehot_encoder.fit_transform(df[['Embarked', 'Cabin']])
+
+onehot_df = pd.DataFrame(
+    onehot_encoded,
+    columns=onehot_encoder.get_feature_names_out()
+)
+
+print(onehot_df.head(5))
+
+### merging the final preprocessed dataset
+df_final = pd.concat([df, ordinal_df, onehot_df], axis=1)
+
+df_final = df_final.drop(columns=['Embarked', 'Cabin'])
+
+### final preprocessed dataset
+
+print('\nbefore preprocessing the dataset:')
+print(df.shape)
+
+print('\nafter preprocessing the dataset:')
+print(df_final.shape)
 
